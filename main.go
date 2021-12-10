f38ff4284abc6b9a0f6ff7b34a009bc9814d7f1c
Majeed <majeed.dl@gmail.com>
2021-12-10 23:45:29 +0300

    add some changes

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+)
 
+// Avenger represents a single hero
+type Avenger struct {
+    RealName string `json:"real_name"`
+    HeroName string `json:"hero_name"`
+    Planet   string `json:"planet"`
+    Alive    bool   `json:"alive"`
+}
+
+func (a *Avenger) isAlive() {
+    a.Alive = true
+}
+
+func main() {
+    avengers := []Avenger{
+        {
+            RealName: "Dr. Bruce Banner",
+            HeroName: "Hulk",
+            Planet:   "Midgard",
+        },
+        {
+            RealName: "Tony Stark",
+            HeroName: "Iron Man",
+            Planet:   "Midgard",
+        },
+        {
+            RealName: "Thor Odinson",
+            HeroName: "Thor",
+            Planet:   "Midgard",
+        },
+    }
+
+    avengers[1].isAlive()
+
+    jsonBytes, err := json.Marshal(avengers)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    fmt.Println(string(jsonBytes))
 }
