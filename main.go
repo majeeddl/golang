3242b451eb81dfd2c5f9cd4561062892488e162b
Majeed <majeed.dl@gmail.com>
2021-12-11 00:42:02 +0300

    add mongodb library

--- main.go
+++ main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+
+	println(uri)
+
+	//client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//coll := client.Database("sample_mflix").Collection("movies")
+	//
+	//println(coll)
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
