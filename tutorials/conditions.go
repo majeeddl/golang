c56bdadf6da8b336fb0574f170a6944b0374b10f
majeed <majeed.dl@gmail.com>
2022-01-03 13:13:20 +0300

    conditions

---
+++ tutorials/conditions.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	fmt.Print("Conditions")
+
+	if 20 > 80 {
+		fmt.Println(" 20 is greater than 18")
+	}else if 10 > 20 {
+		
+	}else {
+		
+	}
+
+}
+
