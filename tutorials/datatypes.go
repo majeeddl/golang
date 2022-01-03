146b078b7541f0c39939a95e2206e92c4a8dbef2
majeed <majeed.dl@gmail.com>
2022-01-03 11:35:52 +0300

    data types are done

---
+++ tutorials/datatypes.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	fmt.Print("Data Types","\n")
+
+	var a bool = true;
+	var b int = 10
+	var c float32 = 12.45
+	var d string ="hello world"
+
+	fmt.Println(a,b,c,d)
+}
