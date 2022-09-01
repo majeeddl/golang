2d97f981843f19193ba8e4909612c504cb9f3cfe
majeed <majeed.dl@gmail.com>
2022-09-01 11:28:32 +0300

    read deck from file

--- golang-learning1/cards/deck.go
+++ golang-learning1/cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -37,9 +38,21 @@ func deal(d deck, handSize int) (deck, deck) {
 }
 
 func (d deck) toString() string {
-	return strings.Join([]string(d), " * ")
+	return strings.Join([]string(d), ",")
 }
 
 func (d deck) saveToFile(filename string) {
 	ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string ) deck{
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error : " , err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs),",")
+
+	return deck(s)
 }
