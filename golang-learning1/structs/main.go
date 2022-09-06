4196952a7ac1fe70f201329f07bbf5707285dc80
majeed <majeed.dl@gmail.com>
2022-09-06 11:51:48 +0300

    refrence type in functions

--- golang-learning1/structs/main.go
+++ golang-learning1/structs/main.go
@@ -2,32 +2,50 @@ package main
 
 import "fmt"
 
-
-type contactInfo struct{
-	email string
+type contactInfo struct {
+	email   string
 	zipCode int
 }
 
-type person struct{
-	firstName string
-	lastName string
-	contactInfo contactInfo
+type person struct {
+	firstName   string
+	lastName    string
+	contactInfo contactInfo // is same with contactInfo
 }
 
-func main(){
+func main() {
 
-	newPerson := person{ firstName: "majeed",lastName: "d"  ,contactInfo: contactInfo{ email: "majeed.dl@gmail.com" , zipCode: 12}}
+	newPerson := person{firstName: "majeed", lastName: "d", contactInfo: contactInfo{email: "majeed.dl@gmail.com", zipCode: 12}}
 
 	fmt.Println(newPerson)
 
 	var newPersonTwo person
 
-	fmt.Printf("%+v",newPersonTwo)
+	fmt.Printf("%+v", newPersonTwo)
 
 	newPersonTwo.firstName = "Ali"
 	newPersonTwo.lastName = "d"
-	newPersonTwo.contactInfo = contactInfo{ email: "ali@gmail.com", zipCode:  12}
+	newPersonTwo.contactInfo = contactInfo{email: "ali@gmail.com", zipCode: 12}
 
 	fmt.Println(newPersonTwo)
 
+	newPersonTwo.print()
+
+	// newPersonTwoPointer := &newPersonTwo;
+	newPersonTwo.updateName("John")
+
+	newPersonTwo.print()
+
+	name := "Bill"
+ 
+    fmt.Println(*&name)
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
 }
