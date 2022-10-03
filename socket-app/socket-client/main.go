6ac59b5140a78948ee26a263f82f5c9873ec23e6
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-03 17:57:49 +0300

    add latest changes

--- socket-app/socket-client/main.go
+++ socket-app/socket-client/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
 func main() {
 	fmt.Println("Hello Socket Client ")
+
+	conn, _ := net.Dial("tcp4", "wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
+
+	//reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Text to send: ")
+	//text, _ := reader.ReadString('\n')
+	//// send to server
+	//fmt.Fprintf(conn, text+"\n")
+	// wait for reply
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + message)
+
 }
