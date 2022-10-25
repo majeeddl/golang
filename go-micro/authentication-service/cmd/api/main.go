707f47814ac589e7948090f90b7ab5f9a18be1fb
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-25 16:32:40 +0300

    basic of authentication service

--- go-micro/authentication-service/cmd/api/main.go
+++ go-micro/authentication-service/cmd/api/main.go
@@ -1,15 +1,37 @@
-
 package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 const (
 	webPort = "80"
 )
 
-// type Config struct{
-// 	DB *sql.DB
-// 	Models data.Models
-// }
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
 
-func main(){
+func main() {
+	log.Println("Start authentication service")
 
+	//TODO : connect to database
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
 }
