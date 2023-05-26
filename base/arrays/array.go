7be32fe2f63cd777cba6c859ee34625964169bfc
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-05-26 10:35:12 +0300

    clean up

--- base/arrays/array.go
+++ base/arrays/array.go
@@ -16,28 +16,28 @@ func PrintArrays() {
 	fmt.Println(a)
 
 	fmt.Println(">>> print a string array")
-	b := [3]string { "ali" , "john" , "majeed"}
+	b := [3]string{"ali", "john", "majeed"}
 
 	fmt.Println(b)
 
 	fmt.Println(">>> slices ")
 
-	c := []int { 10, 20}
+	c := []int{10, 20}
 
 	fmt.Println(c)
-	fmt.Println("len of c =>" , (len(c)))
+	fmt.Println("len of c =>", len(c))
 
-	c = append(c, 1000,2000)
+	c = append(c, 1000, 2000)
 
 	fmt.Println(c)
-	fmt.Println("len of c =>" , (len(c)))
+	fmt.Println("len of c =>", len(c))
 
 	index := 2
 
-	c = append(c[:index],c[index+1:]...)
+	c = append(c[:index], c[index+1:]...)
 
 	fmt.Println(c)
-	fmt.Println("len of c =>" , (len(c)))
+	fmt.Println("len of c =>", len(c))
 
 	fmt.Println("***  end arrays ***")
 	fmt.Println("*******************")
