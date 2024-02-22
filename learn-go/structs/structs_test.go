5e244e27f7af2ef0d7c25bbe772a6bb62160f86a
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2024-02-22 14:59:05 +0300

    feat: implement basic of structs

---
+++ learn-go/structs/structs_test.go
@@ -0,0 +1,45 @@
+package structs
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestPerimeter(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("%#v got %g want %g", shape, got, want)
+		}
+	}
+
+	t.Run(("rectangles"), func(t *testing.T) {
+
+		rectangle := Rectangle{12.0, 6.0}
+		// got := rectangle.Area()
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run(("circles"), func(t *testing.T) {
+
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
