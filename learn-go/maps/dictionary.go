a7e3fe94887a5d6e1064121b2a390cb6869c1e90
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2024-02-26 10:05:48 +0300

    feat: implement maps

---
+++ learn-go/maps/dictionary.go
@@ -0,0 +1,76 @@
+package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound         = errors.New("could not find the word you were looking for")
+	ErrWordExists       = errors.New("word already exists in the dictionary")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrNotFound
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+
+	return nil
+}
