43bc79f26be7072d9530b35987c830607b7be8c4
Majeed Dourandeesh <majeed.dl@gmail.com>
2024-05-23 18:09:06 +0300

    add more examples

---
+++ base/28-channel-directions/channel-dir.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	msg := "passed message"
+
+	ping(pings, msg)
+	pong(pings, pongs)
+
+	receivedMsg := <-pongs
+
+	fmt.Println(receivedMsg)
+
+}
